da139722d6811ce76afc3afafc79dec6a1f68ada
josh <joshuarvlcb@gmail.com>
2021-01-07 22:44:18 -0700

    finished the project was a little tricky

--- script.js
+++ script.js
@@ -5,20 +5,30 @@ const submit = document.getElementById("submit");
 //1. I need to make the values of the input into a object
 
 //2.What i need to do
-//
+//need to make data into a object
 
-const surveyObject = function () {
+//javascript is passing the click event to survey object
+let counter = 0;
+const surveyObject = function (evClick) {
+  evClick.preventDefault(); //stoping form submitting because it refreshes and lose everything
+
   let firstName = document.getElementById("firstName").value;
   let lastName = document.getElementById("lastName").value;
   let email = document.getElementById("email").value;
   let phoneNumber = document.getElementById("phone").value;
-  console.log(
-    (firstName = {
-      fn: firstName,
-      ln: lastName,
-      em: email,
-      phone: phoneNumber,
-    })
-  );
+
+  let survey = {
+    fn: firstName,
+    ln: lastName,
+    em: email,
+    phone: phoneNumber,
+  };
+
+  document.querySelector("form").reset(); //resetting form but not refreshing page so we keep the data
+  counter++;
+  console.log(`Object data for entry ${counter}`);
+  console.log(survey);
 };
-submit.addEventListener("click", surveyObject);
+document.addEventListener("DOMContentLoaded", () => {
+  submit.addEventListener("click", surveyObject);
+});
