36ccdb80891fbb21cb15ebb83fec1e5d45b2ab49
josh <joshuarvlcb@gmail.com>
2021-01-22 13:40:16 -0700

    almost done

--- airplane.js
+++ airplane.js
@@ -1,24 +1,17 @@
 "use strict";
-//You will take the bit of code that we did together and add to it. We already have a way to take the value from text areas and store them inside of an object, and assign them a unique ID value.
-// * First Name
-// * Last Name
-// * Date of Birth (format MM/DD/YYYY)
-// * City they are leaving from
-// * City they are going to
-// * Date leaving (format MM/DD/YYYY)
-// * Date returning(format MM/DD/YYYY)
-// Bags they are bringing (Number)
-// Type of meal - chicken, fish, vegetarian (radio button)
-// Extras - legroom, window seat, headphones, and/or extra food (checkboxes)
+//4 The array should be able to be printed out so you can see a long list of every passenger that is boarding, but only show the ID and Name of the passenger.
 
-// From that information you will also add a few more bits of information that they don’t need to input:
+//function to check hpw long the passenger will be gone for
 
-// Can they drink alcohol? (over 21)
-// Extra costs (+10 for each checkbox +20 for each bag they are bringing)
-// How long will they be leaving for (returning - leaving)
-// Assign a unique ID value
-
-// All the data above should be stored in an object that is stored in an array with proper key values and naming conventions. Make sure you are using a class, to create the passenger objects. The array should be able to be printed out so you can see a long list of every passenger that is boarding, but only show the ID and Name of the passenger.
+const leaveTime = function (dateLeave, dateReturn) {
+  const dateLeave2 = new Date(dateLeave);
+  const dateReturn2 = new Date(dateReturn);
+  const difference = dateReturn2 - dateLeave2;
+  const days = 1000 * 60 * 60 * 24;
+  console.log(
+    `days you have been gone for ${Math.abs(Math.floor(difference / days))}`
+  );
+};
 
 class User {
   constructor(
@@ -30,8 +23,8 @@ class User {
     dateLeave,
     dateReturn,
     id,
-    bags,
     radio,
+    bags,
     extras
   ) {
     this.firstName = firstName;
@@ -47,16 +40,38 @@ class User {
     this.extras = extras;
   }
 }
-let id = 0;
+let id = 55;
 const objectData = [];
 
 //1.i need to get the value of my radio button and put it inside the object array
+//i need to slice the last values of the radio array and always update it
+let radio = [];
+let radioValue = [];
 let radios = document.getElementsByName("project");
 for (let i = 0; i < radios.length; i++) {
-  if (radios[i].checked) {
-    console.log(radios[i].value);
-    break;
-  }
+  radios[i].addEventListener("click", () => {
+    radio.push(radios[i].value);
+    radioValue = radio.slice(-1);
+  });
+}
+
+const extraCost = function (bags, checkbox) {
+  const bags2 = Number(bags);
+  const checkbox2 = checkbox.size;
+  const total = bags2 * 20 + checkbox2 * 10;
+  console.log(`your total is ${total}`);
+};
+
+let boxes = new Set([]);
+let checks = document.getElementsByClassName("checkboxs");
+for (let i = 0; i < checks.length; i++) {
+  checks[i].addEventListener("click", () => {
+    if (checks[i].checked == true) {
+      boxes.add(checks[i].value);
+    } else {
+      boxes.delete(checks[i].value);
+    }
+  });
 }
 
 const dateFormat = function (birthYear) {
@@ -66,9 +81,25 @@ const dateFormat = function (birthYear) {
   let day = date.getDate();
   return `${month}/${day}/${year}`;
 };
+
+const age = function (birthDay2) {
+  const current = new Date();
+  const currentYear = current.getFullYear();
+  const currentMonth = current.getMonth() + 1;
+  const birthDay = new Date(birthDay2);
+  const year = birthDay.getFullYear();
+  const age = currentYear - year;
+  console.log(`you are ${age} years old and ${currentMonth} months old`);
+  if (age >= 21) {
+    return "you can drink alcohol";
+  } else {
+    return "you can only drink juice";
+  }
+};
 const setup = document
   .getElementById("submit5")
-  .addEventListener("click", () => {
+  .addEventListener("click", (e) => {
+    e.preventDefault();
     const firstName = document.getElementById("first").value;
     const lastName = document.getElementById("last").value;
     const birthDay = document.getElementById("birth").value;
@@ -76,6 +107,7 @@ const setup = document
     const cityReturn = document.getElementById("cityReturn").value;
     const dateLeave = document.getElementById("dateLeave").value;
     const dateReturn = document.getElementById("dateReturn").value;
+    const bags = document.getElementById("bagNumbers").value;
     id++;
     const passenger = new User(
       firstName,
@@ -85,8 +117,14 @@ const setup = document
       cityLeave,
       dateFormat(dateLeave),
       dateFormat(dateReturn),
-      id
+      "LX" + id,
+      radioValue,
+      bags,
+      boxes
     );
+    leaveTime(dateReturn, dateLeave);
+    age(birthDay);
+    extraCost(bags, boxes);
     document.querySelector("form").reset();
     if (firstName !== " " && lastName !== " ") {
       objectData.push(passenger);
